f4c5315142b43859d04a2b26f7466af622ec842e
Davorus <67279830+Davorus@users.noreply.github.com>
2024-05-08 11:39:39 +0200

    Trying to set up a data simulator

--- eego-sdk-pybind11-master/Tests/Textfile_writer_test.py
+++ eego-sdk-pybind11-master/Tests/Textfile_writer_test.py
@@ -1,9 +1,12 @@
 import random
 
 with open("eeg_simulation_data.txt", "w") as f:
-    file_values: list = []
-    for it in range(0, 8): 
-        file_values.append(random.uniform(-3, 3))
-        print("Random generated values: " + str(file_values[it]))
-        last_cell_info = 1181.0000000000000000 + it
-    f.write(str(file_values[it]) + " " )
+    for i in range(0, 51):
+        file_values: list = []
+        output: str = ""
+        for it in range(0, 8): 
+            file_values.append(random.uniform(-1, 1))
+            output = output + str(file_values[it]) + " "
+            # last_cell_info = 1181.0000000000000000 + it
+        output = output + str(0.0000000000000000) + " " + str(1181.0000000000000000 + i) + "\n"
+        f.write(output)
