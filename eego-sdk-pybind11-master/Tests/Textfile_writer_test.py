cff4be455af3cf3f49193350d4fd0aee0aef1817
Davorus <67279830+Davorus@users.noreply.github.com>
2024-05-08 11:11:22 +0200

    Small changes

--- eego-sdk-pybind11-master/Tests/Textfile_writer_test.py
+++ eego-sdk-pybind11-master/Tests/Textfile_writer_test.py
@@ -6,4 +6,4 @@ with open("eeg_simulation_data.txt", "w") as f:
         file_values.append(random.uniform(-3, 3))
         print("Random generated values: " + str(file_values[it]))
         last_cell_info = 1181.0000000000000000 + it
-        f.write(str(file_values[it]) + " " )
+    f.write(str(file_values[it]) + " " )
