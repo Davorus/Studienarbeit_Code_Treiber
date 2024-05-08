1d07075d23bee1e0523b08d6fc08d7d1e3d31f69
Davorus <67279830+Davorus@users.noreply.github.com>
2024-05-08 14:27:14 +0200

    Visualisierung komplizierter als gedacht

--- eego-sdk-pybind11-master/File_Reader.py
+++ eego-sdk-pybind11-master/File_Reader.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import time
 from loguru import logger
+from Visualizer import *
 
 """ file_reader Klasse
 Die Visualizer Klasse selbst ist nur für die Darstellung zuständig. Mehr soll diese nicht machen, nur plotten
@@ -11,15 +12,15 @@ logger.add("File_Reader.log")
 class File_Reader():
     def __init__(self) -> None:
         self.file_ptr = open("eeg_simulation_data.txt", "r")
-        self.line_ptr = 0
-        self.amount_lines = 0
         open("File_Reader.log", "w").close() # empties log file
+        self.va = Visualizer()
 
     def read_line_of_file(self):
         for line in pd.read_csv("./eeg_simulation_data.txt", sep=" ", chunksize=1, header=None): # now it should read from the simulation stream
-            print(line) # [] indexing possible
+            # print(line) # [] indexing possible
             logger.info(f"{line}")
-            time.sleep(0.01)
+            self.va.visualize(line)
+            time.sleep(0.1)
             
 
 
--- eego-sdk-pybind11-master/Visualizer.py
+++ eego-sdk-pybind11-master/Visualizer.py
@@ -1,12 +1,21 @@
 import matplotlib as mplt
+from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
 
 """ Visualizer Klasse
 Die Visualizer Klasse selbst ist nur für die Darstellung zuständig. Mehr soll diese nicht machen, nur plotten und eine schöne Oberfläche bieten
 """
 
 class Visualizer():
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self.fig, self.ax = plt.subplots()
 
-
-
+    def visualize(self, data):
+        self.graph = self.ax.plot(data, color="g")[0]
+        plt.ylim(0, 10)
+        def update(frame):
+            self.graph.set_xdata(data)
+            self.graph.set_ydata(3)
+            plt.xlim(data[1], data[0])
+        anim = FuncAnimation(self.fig, update, frames=None)
+        plt.show()
