7843dbe442365317c42225c8512b556211080bd9
Davorus <67279830+Davorus@users.noreply.github.com>
2024-05-08 12:19:06 +0200

    Klassenstruktur angepasst

--- eego-sdk-pybind11-master/File_Reader.py
+++ eego-sdk-pybind11-master/File_Reader.py
@@ -1,7 +1,12 @@
+import pandas as pd
+
 """ file_reader Klasse
 Die Visualizer Klasse selbst ist nur für die Darstellung zuständig. Mehr soll diese nicht machen, nur plotten
 """
 
 class File_Reader():
     def __init__(self) -> None:
-        pass
+        self.file_ptr = open("eeg_simulation_data.txt", "r")
+
+    def read_line_from_file(self):
+        pd.
--- eego-sdk-pybind11-master/Visualizer.py
+++ eego-sdk-pybind11-master/Visualizer.py
@@ -1,11 +1,12 @@
 import matplotlib as mplt
 
 """ Visualizer Klasse
-Die Visualizer Klasse selbst ist nur für die Darstellung zuständig. Mehr soll diese nicht machen, nur plotten
+Die Visualizer Klasse selbst ist nur für die Darstellung zuständig. Mehr soll diese nicht machen, nur plotten und eine schöne Oberfläche bieten
 """
 
 class Visualizer():
     def __init__(self) -> None:
         pass
 
-    
+
+
