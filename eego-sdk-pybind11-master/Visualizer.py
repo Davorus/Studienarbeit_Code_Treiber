3957403842cf30d6947fd7e269b7bc9c03b80420
Davorus <67279830+Davorus@users.noreply.github.com>
2024-05-15 12:02:42 +0200

    Both plotted clean

--- eego-sdk-pybind11-master/Visualizer.py
+++ eego-sdk-pybind11-master/Visualizer.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-from collections import deque
 
 """ Visualizer Klasse
 Die Visualizer Klasse selbst ist nur für die Darstellung zuständig. Mehr soll diese nicht machen, nur plotten und eine schöne Oberfläche bieten
@@ -27,18 +26,32 @@ class Visualizer():
         # title set beta
         self.plot_beta.set_title("Beta Daten")
         
+    def visualize(self, alpha, beta):
+        # appending alpha array
+        if len(self.que_alpha) > self.max_data_length:
+            self.que_alpha.pop(0)
+            self.que_alpha.append(alpha)
+        else:
+            self.que_alpha.append(alpha)
+        
+        # appending beta array
+        if len(self.que_beta) > self.max_data_length:
+            self.que_beta.pop(0)
+            self.que_beta.append(beta)
+        else:
+            self.que_beta.append(beta)
+        
+        # clear plot alpha
+        self.plot_alpha.clear()
+
+        # clear plot alpha
+        self.plot_beta.clear()
+        
         # inverting x_axis, alpha
         self.plot_alpha.invert_xaxis()
 
         # inverting x_axis, beta
         self.plot_beta.invert_xaxis()
-        
-    def visualize(self, alpha, beta):
-        # appending alpha array
-        self.que_alpha.append(alpha)
-        
-        # appending beta array
-        self.que_beta.append(beta)
 
         # plotting alpha data
         self.plot_alpha.plot(self.que_alpha, color="b")
